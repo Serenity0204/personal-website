1a5beda16818169e8bf86f8c9c082863bf3c328b
serenity0204 <yuhenglin0204@gmail.com>
2023-01-11 18:26:37 -0800

    added basic structure

---
+++ portfolio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    detail = models.TextField()
+    github_link = models.CharField()
+    
